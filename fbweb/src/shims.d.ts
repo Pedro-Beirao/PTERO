6307f8da31516a57422df2c828a4855480d095a2
Pedro Beirao <pedrocbeirao@gmail.com>
2026-02-05 15:40:14 +0000

    Remove rete

--- fbweb/src/shims.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '*.svelte' {
-    import type { Component } from 'svelte'
-
-    // eslint-disable-next-line init-declarations
-    const component: Component
-
-    export default component
-  }
