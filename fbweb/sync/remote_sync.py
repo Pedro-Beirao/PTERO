ecd88aadfac1995023c20cc915d5daeb4f54b266
Pedro Beirao <pedrocbeirao@gmail.com>
2026-05-12 23:55:38 +0100

    [app] fix sync script

--- fbweb/sync/remote_sync.py
+++ fbweb/sync/remote_sync.py
@@ -5,9 +5,10 @@ import os
 
 class RemoteSync(base_sync.BaseSync):
 
-    def __init__(self, master_fbs_path, address, path, username, password):
+    def __init__(self, master_fbs_path, address, port, path, username, password):
         self.master_fbs_path = master_fbs_path
         self.address = address
+        self.port = port
         self.path = path
         self.username = username
         self.password = password
@@ -17,7 +18,7 @@ class RemoteSync(base_sync.BaseSync):
             client = paramiko.SSHClient()
             client.load_system_host_keys()
             client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            client.connect(self.address, 22, self.username, self.password) # use port 22 for ssh
+            client.connect(self.address, self.port, self.username, self.password) # use port 22 for ssh
             # delete fbs folder
             command = 'rm -rf ' + self.path
             client.exec_command(command)
@@ -39,19 +40,19 @@ class RemoteSync(base_sync.BaseSync):
             client = paramiko.SSHClient()
             client.load_system_host_keys()
             client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            client.connect(self.address, 22, self.username, self.password) # use port 22 for ssh
-            
-            t = paramiko.Transport((self.address, 22))
+            client.connect(self.address, self.port, self.username, self.password)
+
+            t = paramiko.Transport((self.address, self.port))
             t.connect(
                 username=self.username,
                 password=self.password
             )
-            
+
             sftp = paramiko.SFTPClient.from_transport(t)
 
             # get list of local fbs
             scanned_fbs = self.scantree(self.master_fbs_path)
-            
+
             for fb in scanned_fbs:
                 correct_path = fb.path.replace('\\', '/')
                 dest_path = self.path + correct_path.replace(self.master_fbs_path, '')
@@ -60,7 +61,7 @@ class RemoteSync(base_sync.BaseSync):
                 except FileNotFoundError:
                     # file does not exist on remote
                     self.force_put(sftp, correct_path, dest_path, self.path)
-                else: 
+                else:
                     if fb.stat().st_mtime > dest_fb_stats.st_mtime:
                         # local file is updated
                         sftp.put(correct_path, dest_path)
@@ -106,8 +107,5 @@ class RemoteSync(base_sync.BaseSync):
                 except IOError:
                     # directory already exists
                     pass
-                
+
         sftp.put(src, trgt)
-
-
-    
